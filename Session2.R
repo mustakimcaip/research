a822f5cd130e36c66bc876d0d03cce91e0303acc
mustakimcaip <mustakimcaip@gmail.com>
2026-08-08 23:22:08 +0600

    Add files via upload

---
+++ Session2.R
@@ -0,0 +1,72 @@
+# Using R as calculator (CLT + Enter)
+2 + 2
+5 - 3
+6/3
+
+# Variables 
+# Create a variable using left-ward operator (<-)
+# var_name <- value 
+a <- 10
+
+# Variable naming convention 
+a <- 30
+age <- 30
+my_age <- 30
+exam_score <- 100
+
+# Data types 
+# Numbers 
+age <- 40
+height <- 1.5
+class(age)
+class(height)
+
+# Text / Characters
+name <- "Rahim"
+class(name)
+
+# Logical (TRUE, FALSE)
+is_weekday <- TRUE
+class(is_weekday)
+
+# Arithmetic operators 
+num1 <- 10
+num2 <- 2
+
+
+num1 + num2
+
+num1 - num2
+
+
+
+
+num1 * num2
+num1 / num2
+
+# if num/2; remainder == 0: even, if not: odd 
+10%%2
+11%%2
+
+# Comparative Operators (TRUE, FALSE)
+2 == 2
+4 > 2
+3 < 2 
+3 <= 2 # 3 < 2 OR 3 == 2
+5 >= 5 # 5 > 5 OR 5 == 5
+
+# Multiple Statements / Condition 
+# Logical AND, OR, NOT 
+# AND (&&) - SHIFT + 7 
+2 == 2 && 4 > 2 
+2 == 2 && 4 < 2 
+
+# OR (|) - pipe (Enter + immediate key of enter)
+3 < 2 | 3 == 2
+2 == 2|4 < 2 
+
+# NOT 
+!2 == 2
+
+
+
