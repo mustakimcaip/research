09770a193ccb64b9c3bedfd795a864db50d71a78
mustakimcaip <mustakimcaip@gmail.com>
2026-08-17 12:46:53 +0600

    add session 3

---
+++ Session3.R
@@ -0,0 +1,18 @@
+# Session 3 will open soon
+# syntax
+ if (condition){
+   # decsion (if block)
+ }
+ 
+ # example
+num >- 10
+if (num > 0){
+  print("positive")
+} else {
+  print("Negative")
+}
+ 
+
+
+i = 10
+if (i %% 2 == )
